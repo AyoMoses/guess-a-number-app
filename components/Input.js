838bca07c66999f9fd17d9a5fcf9f274b9f65e41
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2019-11-20 15:10:17 +0100

    game app with fixes

--- components/Input.js
+++ components/Input.js
@@ -1,28 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import {TextInput, StyleSheet} from 'react-native';
-
-
+import { TextInput, StyleSheet } from "react-native";
 
 // A FUNCTIONAL COMPONENT
 
 const Input = props => {
-    return <TextInput {...props} style={{...styles.input, ...props.style}} />
+  return <TextInput {...props} style={{ ...styles.input, ...props.style }} />;
 };
 
-// ADDING ...PROPS ALLOW US TO BE ABLE TO OVERRIDE THE COMPONENT PROPS AND FORWARD IT 
-
+// ADDING ...PROPS ALLOW US TO BE ABLE TO OVERRIDE THE COMPONENT PROPS AND FORWARD IT
 
 const styles = StyleSheet.create({
-    input: {
-        
-        height: 30,
-        borderBottomColor: 'grey',
-        borderBottomWidth: 1,
-        paddingVertical: 5
-    }
+  input: {
+    height: 30,
+    borderBottomColor: "grey",
+    borderBottomWidth: 1,
+    paddingVertical: 5
+  }
 });
-
-
 
 export default Input;
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
 
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
@@ -21,15 +21,73 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(
     generatedRandomBetween(1, 100, props.userChoice)
   );
+  
+  const [rounds, setRounds] = useState(0);
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
 
-  return(
-    <View>
-        <Text>Oppoent's Guess</Text>
+  // WE USE DESTRUCTURING FOR OBJECTS
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if(currentGuess === userChoice) {
+      onGameOver(rounds);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
+
+  const nextGuessHandler = direction => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Fear God abeg!", "U sabi say ur input wrong die...", [
+        { text: "Nor Vez!", style: "cancel" }
+      ]);
+
+      return;
+    }
+    
+
+    if (direction === 'lower') {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess;
+    }
+    const nextNumber = generatedRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+    setCurrentGuess(nextNumber);
+
+    setRounds(curRounds => curRounds + 1);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text>Oppoent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={nextGuessHandler.bind(this, "lower")} />
+        <Button
+          title="GREATER"
+          onPress={nextGuessHandler.bind(this, "greater")}
+        />
+      </Card>
     </View>
-    <NumberContainer>{currentGuess}</NumberContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
+  }
+});
 
 export default GameScreen;
